99032142a2c1fc2f2c0a05b80bab2506fcb321ba
lynnsamuelson <lynn.samuelson@gmail.com>
2015-10-23 15:30:48 -0500

    Implementation of even numbers

---
+++ Mathmagician/EvenNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class EvenNumbers: NaturalNumbers
+    {
+        public EvenNumbers()
+        {
+            first = 2;
+        }
+        public override int GetNext(int x)
+        {
+            //return x + 2;
+            return base.GetNext(x) + 1;//since GetNext is x+1 then another + 1 = x+2
+        }
+    }
+}
