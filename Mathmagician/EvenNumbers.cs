0153c6eaf434dc28119e3f1f7cd24a6ffcd136cf
lynnsamuelson <lynn.samuelson@gmail.com>
2015-10-23 15:48:18 -0500

    deleted Getnext override and use parents increment data member.

--- Mathmagician/EvenNumbers.cs
+++ Mathmagician/EvenNumbers.cs
@@ -11,11 +11,12 @@ namespace Mathmagician
         public EvenNumbers()
         {
             first = 2;
+            increment = 2;
         }
-        public override int GetNext(int x)
-        {
+        ////public override int GetNext(int x)
+       // {
             //return x + 2;
-            return base.GetNext(x) + 1;//since GetNext in NaturalNumbers is x+1 then another + 1 = x+2
-        }
+           // return base.GetNext(x) + 1;//since GetNext in NaturalNumbers is x+1 then another + 1 = x+2
+        //}
     }
 }
